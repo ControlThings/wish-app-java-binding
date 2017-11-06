669d00b13cec2c12f97a0a326da2dbbb1eb7f1d7
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2017-11-06 11:48:14 +0200

    more files

---
+++ Errors.java
@@ -0,0 +1,127 @@
+package wishApp;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import mistNode.RequestInterface;
+
+
+/**
+ * Created by jeppe on 3/2/17.
+ */
+public class Errors {
+
+    private static Errors instance;
+
+    static Errors getInstance() {
+        if (instance == null) {
+            instance = new Errors();
+        }
+        return instance;
+    }
+
+
+    private int id;
+    private HashMap<Integer, MistCb> mistCbHashMap;
+    private HashMap<Integer, WishCb> wishCbHashMap;
+
+    private Errors() {
+        id = 1;
+        mistCbHashMap = new HashMap<>();
+        wishCbHashMap = new HashMap<>();
+        RequestInterface.getInstance().registerMistRpcErrorHandler(new RequestInterface.Error() {
+            @Override
+            public void cb(String op, int code, String msg) {
+                mistError(op, code, msg);
+            }
+        });
+
+        wishApp.RequestInterface.getInstance().registerWishRpcErrorHandler(new wishApp.RequestInterface.Error() {
+            @Override
+            public void cb(String op, int code, String msg) {
+                wishError(op, code, msg);
+            }
+        });
+    }
+
+    private int registerCb(MistCb cb) {
+        mistCbHashMap.put(id, cb);
+        return id++;
+    }
+
+    private int registerCb(WishCb cb) {
+        wishCbHashMap.put(id, cb);
+        return id++;
+    }
+
+    public static int mist(MistCb callback) {
+        return getInstance().registerCb(callback);
+    }
+
+    public static int wish(WishCb callback) {
+        return getInstance().registerCb(callback);
+    }
+
+    public interface MistCb {
+        public void cb(String op, int code, String msg);
+    }
+
+    public interface WishCb {
+        public void cb(String op, int code, String msg);
+    }
+
+    public static void cancel(int id) {
+        if (getInstance().mistCbHashMap.containsKey(id)) {
+            getInstance().mistCbHashMap.remove(id);
+            return;
+        }
+        if (getInstance().wishCbHashMap.containsKey(id)) {
+            getInstance().wishCbHashMap.remove(id);
+            return;
+        }
+    }
+
+    static void mistError(String op, int code, String msg) {
+        Iterator iterator = getInstance().mistCbHashMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            MistCb mistCb = (MistCb) pair.getValue();
+            mistCb.cb(op, code, msg);
+            iterator.remove();
+        }
+    }
+
+    static void wishError(String op, int code, String msg) {
+        Iterator iterator = getInstance().wishCbHashMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            WishCb wishCb = (WishCb) pair.getValue();
+            wishCb.cb(op, code, msg);
+            iterator.remove();
+        }
+    }
+
+   public static void mistError(String op, int code, String msg, byte[] bson) {
+        //bsonConsolePrettyPrinter("BSON code: " + code, bson);
+        Iterator iterator = getInstance().mistCbHashMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            MistCb mistCb = (MistCb) pair.getValue();
+            mistCb.cb(op, code, msg);
+            iterator.remove();
+        }
+    }
+
+    public static void wishError(String op, int code, String msg, byte[] bson) {
+        //bsonConsolePrettyPrinter("BSON code: " + code, bson);
+        Iterator iterator = getInstance().wishCbHashMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            WishCb wishCb = (WishCb) pair.getValue();
+            wishCb.cb(op, code, msg);
+            iterator.remove();
+        }
+    }
+
+}
---
+++ WishApp.java
@@ -0,0 +1,160 @@
+package wishApp;
+
+import android.content.Context;
+
+import addon.WishFile;
+
+
+/**
+ * Created by jan on 11/1/16.
+ */
+
+public class WishApp {
+    private Context context;
+    private WishFile file;
+
+    static {
+        System.loadLibrary("mist");
+    }
+
+    /* Private constructor must exist to enforce Singleton pattern */
+    private WishApp() {}
+
+
+    private static class MistNodeHolder {
+        private static final WishApp INSTANCE = new WishApp();
+    }
+
+    public static WishApp getInstance() {
+        return MistNodeHolder.INSTANCE;
+    }
+
+    public void startWishApp(Context context) {
+        this.context = context;
+        file = new WishFile(context);
+
+        String appName = context.getPackageName();
+        if (appName.length() > 32) {
+            appName = appName.substring(0, 32);
+        }
+        startWishApp(appName);
+    }
+
+    synchronized native void startWishApp(String appName);
+    synchronized native void stopWishApp();
+
+    /**
+     * Send a Wish request to the local Wish core
+     * @param req the request in BSON format
+     * @return the RPC id; The invalid RPC id 0 is returned for any errors.
+     */
+    public synchronized native int request(byte[] req, RequestCb cb); //will call  wish_app_core_with_cb_context
+
+    public synchronized native int requestCancel(int id);
+
+
+    void online(byte[] peerBson) {
+
+    }
+
+    void offline(byte[] peerBson) {
+
+    }
+
+    /**
+     * Open a file
+     *
+     * @param filename the filename
+     * @return the file ID number which can be used with read, write..., or -1 for an error
+     */
+    public int openFile(String filename) {
+        return file.open(filename);
+    }
+
+    /**
+     * Close a fileId
+     *
+     * @param fileId the fileId to close
+     * @return 0 for success, -1 for an error
+     */
+    public int closeFile(int fileId) {
+        return file.close(fileId);
+    }
+
+    /**
+     * Read from a file
+     *
+     * @param fileId the fileId, obtained with open()
+     * @param buffer The buffer to place the bytes into
+     * @param count  the number of bytes to read
+     * @return the amount of bytes read, or 0 if EOF, or -1 for an error
+     */
+    public int readFile(int fileId, byte[] buffer, int count) {
+        if (count != buffer.length) {
+            return -1;
+        }
+        return file.read(fileId, buffer, count);
+    }
+
+    /**
+     * Write to a file in internal storage
+     *
+     * @param fileId the fileId
+     * @param buffer the databuffer to be written
+     * @param count  The
+     * @return
+     */
+    public int writeFile(int fileId, byte[] buffer, int count) {
+        if (count != buffer.length) {
+            return -1;
+        } else {
+            return file.write(fileId, buffer);
+        }
+    }
+
+    public int seekFile(int fileId, int offset, int whence) {
+        return (int) file.seek(fileId, offset, whence);
+    }
+
+    public int rename(String oldName, String newName) {
+        return file.rename(oldName, newName);
+    }
+
+    public int remove(String filename) {
+        return file.remove(filename);
+    }
+
+
+    public abstract static class RequestCb {
+
+        /**
+         * The callback invoked when "ack" is received for a RPC request
+         *
+         * @param data a document containing RPC return value as 'data' element
+         */
+        public void ack(byte[] data) {
+            response(data);
+            end();
+        };
+
+        /**
+         * The callback invoked when "sig" is received for a RPC request
+         *
+         * @param data the contents of 'data' element of the RPC reply
+         */
+        public void sig(byte[] data) {
+            response(data);
+        };
+
+        public abstract void response(byte[] data);
+        public abstract void end();
+
+        /**
+         * The callback invoked when "err" is received for a failed RPC request
+         *
+         * @param code the error code
+         * @param msg  a free-text error message
+         */
+        public abstract void err(int code, String msg);
+    }
+}
