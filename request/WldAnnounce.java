e174a9124f203c0f8c496ae43372ffcb2831b92c
Jesper Lökfors <jesper.lokfors@controlthings.fi>
2017-11-22 15:43:57 +0200

    wld.announce added

---
+++ request/WldAnnounce.java
@@ -0,0 +1,68 @@
+package wish.request;
+
+import org.bson.BSONException;
+import org.bson.BsonBinaryWriter;
+import org.bson.BsonDocument;
+import org.bson.BsonWriter;
+import org.bson.RawBsonDocument;
+import org.bson.io.BasicOutputBuffer;
+
+import wish.WishApp;
+
+import static wish.request.Callback.BSON_ERROR_CODE;
+import static wish.request.Callback.BSON_ERROR_STRING;
+
+/**
+ * Created by jeppe on 10/20/16.
+ */
+class WldAnnounce {
+    static int request(Wld.AnnounceCb callback) {
+        final String op = "wld.announce";
+
+        BasicOutputBuffer buffer = new BasicOutputBuffer();
+        BsonWriter writer = new BsonBinaryWriter(buffer);
+        writer.writeStartDocument();
+
+        writer.writeString("op", op);
+
+        writer.writeStartArray("args");
+        writer.writeEndArray();
+
+        writer.writeInt32("id", 0);
+
+        writer.writeEndDocument();
+        writer.flush();
+
+        return WishApp.getInstance().request(buffer.toByteArray(), new WishApp.RequestCb() {
+            Wld.AnnounceCb cb;
+
+            @Override
+            public void response(byte[] data) {
+                try {
+                    BsonDocument bson = new RawBsonDocument(data);
+                    boolean value = bson.getBoolean("data").getValue();
+                    cb.cb(value);
+                } catch (BSONException e) {
+                    cb.err(BSON_ERROR_CODE, BSON_ERROR_STRING);
+                }
+            }
+
+            @Override
+            public void end() {
+                cb.end();
+            }
+
+            @Override
+            public void err(int code, String msg) {
+                super.err(code, msg);
+                cb.err(code, msg);
+            }
+
+            private WishApp.RequestCb init(Wld.AnnounceCb callback) {
+                this.cb = callback;
+                return this;
+            }
+
+        }.init(callback));
+    }
+}
